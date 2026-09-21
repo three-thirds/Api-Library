d3c2dec3a7ecfad602c730f30365053c4b3ccec4
William Gu <williamnumismatics@gmail.com>
2026-09-22 09:12:49 +1000

    got shadcn working

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,16 @@
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import path from 'path'
 import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), tailwindcss()],
+  resolve: {
+    alias: {
+      $lib: path.resolve('./src/lib'),
+    },
+  },
+  server: {
+    proxy: { '/ping': 'http://localhost:8787', '/weather': 'http://localhost:8787' },
+  },
 })
