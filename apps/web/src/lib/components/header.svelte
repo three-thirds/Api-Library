e418b8f46489d73f3577a58709a56b39bb662e10
William Gu <williamnumismatics@gmail.com>
2026-09-23 04:10:50 +0800

    added a const for the links now gonna do the highlighty thing

--- apps/web/src/lib/components/header.svelte
+++ apps/web/src/lib/components/header.svelte
@@ -1,1 +1,18 @@
-hi
+<script lang="ts">
+    import { page } from "$app/stores";
+    import { Button } from "$lib/components/ui/button/index.js";
+
+    const links = [
+        {label: "Home", href: "/"},
+        {label: "Docs", href: "/docs"},
+        {label: "Contribution", href: "/contribution"}
+    ]
+
+    let active = $state(home)
+</script>
+
+<header class="flex items-center gap-2 border-b bg-background/50 p-2">
+    {#each links as link}
+        <Button variant="link">{link}</Button>
+    {/each}
+</header>
