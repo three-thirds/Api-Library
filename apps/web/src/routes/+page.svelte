e42eb05cfb8a9faecf91679d67a28bc3f6c91a8a
William Gu <williamnumismatics@gmail.com>
2026-09-22 10:46:30 +1000

    added tailwindcss and shadcn again gonna test button now

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to threethirds lfkdjlfjkasldkf</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="text-4xl font-bold text-blue-500">
+	ThreeThirds
+</h1>
