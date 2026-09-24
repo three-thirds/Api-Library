d79110c64c29f0b1da6aa6db7fa1df1d47179664
William Gu <williamnumismatics@gmail.com>
2026-09-24 22:48:25 +0800

    added a basic weather api next is gonna be adjustible params

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,8 +1,10 @@
 import { Hono } from 'hono';
 import ping from './routes/ping';
+import weather from './routes/weather';
 
 const api = new Hono();
 
 api.route('/ping', ping);
+api.route('/weather', weather)
 
 export default api;
---
+++ apps/api/src/routes/weather.ts
@@ -0,0 +1,22 @@
+import { Hono } from 'hono';
+
+const app = new Hono();
+const cache = new Map<string, { data: any; expiresAt: number }>();
+const CACHE_TTL_MS = 60 * 1000;
+
+app.get("/", async (c) => {
+    try {
+        const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m");
+
+        if (!response.ok) {
+            return c.json({ error: "Failed to fetch weather data" }, 500);
+        }
+
+            const data = await response.json();
+            return c.json(data);
+    } catch (error) {
+        return c.json({ error: "An error occurred while fetching weather data" }, 500);
+    }
+});
+
+export default app;
