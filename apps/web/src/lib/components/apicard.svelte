2da42cae0a55b9e2e71c21e00ff47ae3955ca770
William Gu <williamnumismatics@gmail.com>
2026-09-24 21:55:37 +0800

    got a very basic pretty bad api card

--- apps/web/src/lib/components/apicard.svelte
+++ apps/web/src/lib/components/apicard.svelte
@@ -4,8 +4,31 @@
 
 <div class="flex flex-col gap-4">
     {#each apis as api (api.id)}
-        <div class="flex flex-col gap-2 rounded-lg border bg-background p-4">
-            
+        <div class="flex flex-row gap-2 rounded-lg border bg-background p-4 justify-between">
+            <div>
+                <h1 class="text-2xl font-semibold">{api.name}</h1>
+                <div class="border border-t border-dotted"></div>
+                <h3 class="text-sm text-muted-foreground mb-3">{api.description}</h3>
+
+                <p class="text-lg">Path Parameters</p>
+                <hr>
+                {#each api.params as param (param.name)}
+                    <div class="flex items-center gap-2">
+                        <p class="text-base text-foreground">{param.name}</p>
+                        <p class="text-xs text-muted-foreground">{param.type}</p>
+                        {#if param.required}
+                            <p class="text-xs text-[#d98014]">Required</p>
+                        {/if}
+                    </div>
+                    <p class="text-sm">{param.description}</p>
+                    <p class="text-sm">Example: {param.example}</p>
+                    <hr>
+                {/each}
+                
+            </div>
+            <div class="flex flex-row gap-2 items-center">
+                hi
+            </div>
         </div>
     {/each}
 
