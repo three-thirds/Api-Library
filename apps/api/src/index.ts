f9695a07989822a8d3857aa950262b030517e4ae
Chinmay Shet <chishxd@gmail.com>
2026-09-25 02:12:52 +0530

    feat: add hackatime /user endpoint

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,11 +2,13 @@ import { Hono } from 'hono';
 import ping from './routes/ping';
 import minecraft from './routes/minecraft.ts';
 import status from './routes/status.ts';
+import hackatime from './routes/hackatime.ts';
 
 const api = new Hono();
 
 api.route('/ping', ping);
 api.route('/minecraft', minecraft);
 api.route('/status', status);
+api.route('/hackatime', hackatime);
 
 export default api;
