296639bce73e703d11b2cb6d5f1cf06355fa77d0
William Gu <williamnumismatics@gmail.com>
2026-09-25 21:10:57 +0800

    basic wikipedia api url

--- apps/api/src/routes/wikipedia.ts
+++ apps/api/src/routes/wikipedia.ts
@@ -2,4 +2,9 @@ import { Hono } from 'hono';
 
 const app = new Hono();
 
+app.get("/", async (c) => {
+    const WIKIPEDIA_API_URL = "https://en.wikipedia.org/w/api.php";
+
+})
+
 export default app;
