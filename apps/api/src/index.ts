5f56ac955bc7d3187201dd11771572f12b343c70
William Gu <williamnumismatics@gmail.com>
2026-09-25 21:07:35 +0800

    got the basic wikipedia stuff working

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,11 +2,13 @@ import { Hono } from 'hono';
 import ping from './routes/ping';
 import weather from './routes/weather';
 import issLocation from './routes/iss-location';
+import wikipedia from './routes/wikipedia';
 
 const api = new Hono();
 
 api.route('/ping', ping);
 api.route('/weather', weather)
 api.route('/iss-location', issLocation);
+api.route('/wikipedia', wikipedia);
 
 export default api;
