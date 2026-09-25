2bc0e193d01bc2e614da893d90a6810397b5b33d
William Gu <williamnumismatics@gmail.com>
2026-09-25 20:59:15 +0800

    finished up a quick ios location

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,12 @@
 import { Hono } from 'hono';
 import ping from './routes/ping';
 import weather from './routes/weather';
+import issLocation from './routes/iss-location';
 
 const api = new Hono();
 
 api.route('/ping', ping);
 api.route('/weather', weather)
+api.route('/iss-location', issLocation);
 
 export default api;
--- apps/api/src/routes/iss-location.ts
+++ apps/api/src/routes/iss-location.ts
@@ -1,1 +1,15 @@
 import { Hono } from 'hono';
+
+const app = new Hono();
+
+app.get("/", async (c) => {
+    const response = await fetch("http://api.open-notify.org/iss-now.json");
+    if (!response.ok) {
+        return c.json({ error: "Failed to fetch ISS location data" }, 502);
+    }
+
+    const data = await response.json();
+    return c.json(data);
+})
+
+export default app;
