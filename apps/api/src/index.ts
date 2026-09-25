910051dff062137965ad9e21d1a231426fe050d1
DevaanshPathak <devaanshpathak08@gmail.com>
2026-09-26 00:50:28 +0530

    add gold route to fetch and handle gold price data

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,8 +1,10 @@
 import { Hono } from 'hono';
 import ping from './routes/ping';
+import gold from './routes/gold';
 
 const api = new Hono();
 
 api.route('/ping', ping);
+api.route('/gold', gold);
 
 export default api;
