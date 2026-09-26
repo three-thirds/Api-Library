ba9e216e323347d3fb20f5d528e16e07e7385638
DevaanshPathak <devaanshpathak08@gmail.com>
2026-09-26 17:24:36 +0530

    added copper route

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -4,6 +4,7 @@ import silver from './routes/silver';
 import oil from './routes/oil';
 import gold from './routes/gold';
 import platinum from './routes/platinum';
+import copper from './routes/copper';
 
 const api = new Hono();
 
@@ -12,5 +13,6 @@ api.route('/silver', silver);
 api.route('/oil', oil);
 api.route('/gold', gold);
 api.route('/platinum', platinum);
+api.route('/copper', copper);
 
 export default api;
