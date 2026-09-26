0de13264976009b0e44921edf9ab3c8ceaa97eec
DevaanshPathak <devaanshpathak08@gmail.com>
2026-09-26 14:36:55 +0530

    mounted oil

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,12 @@
 import { Hono } from 'hono';
 import ping from './routes/ping';
 import silver from './routes/silver';
+import oil from './routes/oil';
 
 const api = new Hono();
 
 api.route('/ping', ping);
 api.route('/silver', silver);
+api.route('/oil', oil);
 
 export default api;
