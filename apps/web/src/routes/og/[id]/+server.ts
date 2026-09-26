f6a39083cb899164bd93f3d908d520f4d2dba5ba
William Gu <williamnumismatics@gmail.com>
2026-09-26 22:22:02 +0800

    screw vercel stuff bye so annoying

--- apps/web/src/routes/og/[id]/+server.ts
+++ apps/web/src/routes/og/[id]/+server.ts
@@ -2,6 +2,10 @@ import { ImageResponse } from "@vercel/og";
 import apis from '$lib/apis.json';
 import type { RequestHandler} from './$types';
 
+export const config = {
+  runtime: 'edge',
+};
+
 export const GET: RequestHandler = async ({ params }) => {
     const api = apis.find((api) => api.id === params.id);
     if (!api) {
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,10 +4,14 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
+	  define: {
+    'globalThis.EdgeRuntime': JSON.stringify('vite-dev'),
+  },
 	resolve: {
 		conditions: ['worker', 'edge-light']
 	},
 	ssr: {
+		noExternal: ['@vercel/og'],
 		resolve: {
 			conditions: ['worker', 'edge-light']
 		}
