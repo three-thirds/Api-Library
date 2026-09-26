cd933ae41bda48edab963499366435fa30fc9716
DevaanshPathak <devaanshpathak08@gmail.com>
2026-09-26 14:12:41 +0530

    mounted silver

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,8 +1,10 @@
 import { Hono } from 'hono';
 import ping from './routes/ping';
+import silver from './routes/silver';
 
 const api = new Hono();
 
 api.route('/ping', ping);
+api.route('/silver', silver);
 
 export default api;
