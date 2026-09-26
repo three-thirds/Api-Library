826a1acda4db54b00ecd63ad723b63dcb18859f7
William Gu <williamnumismatics@gmail.com>
2026-09-26 22:25:24 +0800

    added a test file and added plugin in vite config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
+import { sveltekitOG } from '@ethercorps/sveltekit-og/plugin';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit(), sveltekitOG()]
 });
