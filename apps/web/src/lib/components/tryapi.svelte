84d0de373482e4b4c8ab599a56c4e9175f47a939
William Gu <williamnumismatics@gmail.com>
2026-09-26 09:24:29 +0800

    fixed bugs and starting to add stuff

--- apps/web/src/lib/components/tryapi.svelte
+++ apps/web/src/lib/components/tryapi.svelte
@@ -1,7 +1,26 @@
 <script lang="ts">
  import * as Dialog from "$lib/components/ui/dialog/index.js";
+ import { Button } from "$lib/components/ui/button/index.js";
+ import { Input } from "$lib/components/ui/input/index.js";
  
  let { api}: {api:any} = $props();
+ let open = $state(false);
+ let values = $state<Record<string, string>>(
+    Object.fromEntries((api.params?? []).map((param: any) => [param.name, param.example ?? '']))
+ );
+
+ let loading = $state(false);
+ let responseText = $state('');
+ let status = $state<number | null>(null);
+
+    async function send() {
+        loading = true;
+        responseText: '';
+        status: null;
+    }
+
+    let path = api.route;
+
 </script>
 
 <Dialog.Root>
