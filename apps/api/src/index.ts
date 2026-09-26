d7ffd708c7d63052f47ddec06b718397d9721bc1
DevaanshPathak <devaanshpathak08@gmail.com>
2026-09-26 16:38:29 +0530

    added platinum route

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -3,6 +3,7 @@ import ping from './routes/ping';
 import silver from './routes/silver';
 import oil from './routes/oil';
 import gold from './routes/gold';
+import platinum from './routes/platinum';
 
 const api = new Hono();
 
@@ -10,5 +11,6 @@ api.route('/ping', ping);
 api.route('/silver', silver);
 api.route('/oil', oil);
 api.route('/gold', gold);
+api.route('/platinum', platinum);
 
 export default api;
