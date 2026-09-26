d40aeeab1c936a3eb910023129fa0ef423acdf9c
William Gu <williamnumismatics@gmail.com>
2026-09-26 09:35:27 +0800

    added a import

--- apps/web/src/lib/components/tryapi.svelte
+++ apps/web/src/lib/components/tryapi.svelte
@@ -2,6 +2,7 @@
  import * as Dialog from "$lib/components/ui/dialog/index.js";
  import { Button } from "$lib/components/ui/button/index.js";
  import { Input } from "$lib/components/ui/input/index.js";
+ import { untrack } from "svelte";
  
  let { api}: {api:any} = $props();
  let open = $state(false);
